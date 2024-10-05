c7943bab43caebaf0ef0a048465faf1d18aae8dc
YoonSihyung <beshy3752@naver.com>
2024-10-05 18:56:24 +0000

    1st commit

---
+++ 2021314618_yahtzee.py
@@ -0,0 +1,154 @@
+import random
+
+p1_round = 1
+p1_total_score = 0
+p1_scores = {'Aces': 0, 'Twos': 0, 'Threes': 0, 'Fours': 0, 'Fives': 0, 'Sixes': 0, 'Chance': 0, 'Three of a Kind': 0, 'Four of a Kind': 0, 'Full House': 0, 'Small Straight': 0, 'Large Straight': 0, 'Yahtzee': 0}
+
+p2_round = 1
+p2_score = 0
+p2_aces = 0
+p2_twos = 0
+p2_threes = 0
+p2_fours = 0
+p2_fives = 0
+p2_sixes = 0
+p2_chance = 0
+p2_tok = 0
+p2_fok = 0
+p2_fh = 0
+p2_ss = 0
+p2_ls = 0
+p2_yaz = 0
+
+for i in range(13):
+	print("Player 1: Round %d" % i)
+	print("Choose the option.")
+	print("1. Roll the dice")
+	print("2. Check the score sheet")
+	option = int(input())
+	while True:
+		if option not in [1, 2]:
+			print("You input wrong option. Please enter again.")
+			print("")
+			print("Choose the option.")
+			print("1. Roll the dice")
+			print("2. Check the score sheet")
+			option = int(input())
+		else:
+			break
+	
+	if option == 1:
+		result = random.choices(range(1, 7), k = 5)
+		print("Result of dice")
+		print(result)
+		print("Reroll the dice? [Y/N]", end = '')
+		reroll = input()
+		while True:
+			if reroll not in ['Y', 'N']:
+				print("You input wrong option. Please enter again.")
+				print("")
+				print("Reroll the dice? [Y/N]", end = '')
+				reroll = input()
+			else:
+				break
+		
+		if reroll == 'Y':
+			print("Choose all of dice that you want to keep. Seperate number with space.")
+			print("If you want to change all of the dice, then input '0'")
+			keep = list(map(int, input().split()))
+			
+			if keep[0] == 0 and len(keep) == 1:
+				result = random.choices(range(1, 7), k = 5)
+			else:
+				for j in range(5):
+					if j+1 not in keep:
+						result[j] = random.randint(1, 6)
+
+			print("Result of dice")
+			print(result)
+			print("Reroll the dice? [Y/N]", end = '')
+			reroll = input()
+			while True:
+				if reroll not in ['Y', 'N']:
+					print("Please enter: [Y/N]")
+					reroll = input()
+				else:
+					break
+
+			if reroll == 'Y':
+				print("Choose all of dice that you wnat to keep. Seperate number with space.")
+				print("If you want to change all of the dice, then input '0'")
+				keep = list(map(int, input().split()))
+
+				if keep[0] == 0 and len(keep) == 1:
+					result = random.choices(range(1, 7), k = 5)
+				else:
+					for j in range(5):
+						if j+1 not in keep:
+							result[j] = random.randint(1, 6)
+
+				print("Result of dice")
+				print(result)
+
+			#elif reroll == 'N':
+		max_rep = 1
+		for j in range(5):
+			temp_count = result.count(result[i])
+			if temp_count > max_rep:
+				max_rep = temp_count
+
+		temp_list = sorted(result)
+		ss_check = False
+		ls_check = False
+		if (temp_list[:4] in [[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6]] or temp_list[1:] in [[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6]]):
+			ss_check = True
+		if temp_list in [[1, 2, 3, 4, 5], [2, 3, 4, 5, 6]]:
+			ls_check = True
+
+		p1_aces_expected = result.count(1)
+		p1_twos_expected = result.count(2) * 2
+		p1_threes_expected = result.count(3) * 3
+		p1_fours_expected = result.count(4) * 4
+		p1_fives_expected = result.count(5) * 5
+		p1_sixes_expected = result.count(6) * 6
+		p1_chance_expected = sum(result)
+		p1_tok_expected = sum(result) if max_rep > 3 else 0
+		p1_fok_expected = sum(result) if max_rep > 4 else 0
+		p1_fh_expected = 25 if (len(set(result)) == 2 and max_rep == 3) else 0
+		p1_ss_expected = 30 if ss_check else 0
+		p1_ls_expected = 40 if ls_check else 0
+		p1_yaz_expected = 50 if len(set(result)) == 1 else 0
+
+		print("")
+		print("+-----------------+-------+")
+		print("|    Category     | Score |")
+		print("+-----------------+-------+")
+		print("|      Aces       |%4s   |" % str(p1_aces_expected) if p1_scores['Aces'] == 0 else '-')
+		print("|      Twos       |%4s   |" % str(p1_twos_expected) if p1_scores['Twos'] == 0 else '-')
+		print("|     Threes      |%4s   |" % str(p1_threes_expected) if p1_scores['Threes'] == 0 else '-')
+		print("|      Fours      |%4s   |" % str(p1_fours_expected) if p1_scores['Fours'] == 0 else '-')
+		print("|      Fives      |%4s   |" % str(p1_fives_expected) if p1_scores['Fives'] == 0 else '-')
+		print("|      Sixes      |%4s   |" % str(p1_sixes_expected) if p1_scores['Sixes'] == 0 else '-')
+		print("|     Chance      |%4s   |" % str(p1_chance_expected) if p1_scores['Chance'] == 0 else '-')
+		print("| Three of a Kind |%4s   |" % str(p1_tok_expected) if p1_scores['Three of a Kind'] == 0 else '-')
+		print("| Four of a Kind  |%4s   |" % str(p1_fok_expected) if p1_scores['Four of a Kind'] == 0 else '-')
+		print("|   Full House    |%4s   |" % str(p1_fh_expected) if p1_scores['Full House'] == 0 else '-')
+		print("| Small Straight  |%4s   |" % str(p1_ss_expected) if p1_scores['Small Straight'] == 0 else '-')
+		print("| Large Straight  |%4s   |" % str(p1_ls_expected) if p1_scores['Large Straight'] == 0 else '-')
+		print("|     Yahtzee     |%4s   |" % str(p1_yaz_expected) if p1_scores['Yahtzee'] == 0 else '-')
+		print("+-----------------+-------+")
+		print("")
+		print("Choose a category to record the score")
+		category = input()
+		while True:
+			if category not in p1_scores.keys() or p1_scores[category] != 0:
+				print("You input wrong category. Please enter again.")
+				print("")
+				print("Choose a category to record the score")
+				category = input()
+			else:
+				break
+
+	if option == 2:
+		print("+-----------------+-------+")
+		print("Player 1 Current Score: %d" % p1_total_score)
